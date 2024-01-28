4ec181627a8c977e2940bc6191f7ef1d34650f0d
NP v/d Spek <github_mail@lumensoft.nl>
2024-01-28 05:02:55 +0100

    fix driver loading issue

--- esphome/components/display_driver/drivers.py
+++ esphome/components/display_driver/drivers.py
@@ -54,97 +54,97 @@ def validate_model_registry(base_schema, **kwargs):
     return validator
 
 
-def load_display_driver(model_key):
+async def load_display_driver(model_key):
     registry_item = DISPLAY_REGISTRY[model_key]
-    return {registry_item.type_id, registry_item.fun}
+    return [registry_item.type_id, registry_item.fun]
 
 
 @DISPLAY_REGISTRY.register(
     "M5STACK", display_ns.class_("Display_M5Stack", DisplayDriver), cv.Schema({})
 )
 async def M5STACK_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "M5CORE", display_ns.class_("Display_M5CORE", DisplayDriver), cv.Schema({})
 )
 async def M5CORE_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "TFT_2.4", display_ns.class_("Display_ILI9341", DisplayDriver), cv.Schema({})
 )
 async def TFT_2_4_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "TFT_2.4R", display_ns.class_("Display_ILI9342", DisplayDriver), cv.Schema({})
 )
 async def TFT_2_4R_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9341", display_ns.class_("Display_ILI9341", DisplayDriver), cv.Schema({})
 )
 async def ILI9341_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9342", display_ns.class_("Display_M5CORE", DisplayDriver), cv.Schema({})
 )
 async def ILI9342_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9481-18", display_ns.class_("Display_ILI948118", DisplayDriver), cv.Schema({})
 )
 async def ILI948118_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9486", display_ns.class_("Display_ILI9486", DisplayDriver), cv.Schema({})
 )
 async def ILI9486_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9488", display_ns.class_("Display_ILI9488", DisplayDriver), cv.Schema({})
 )
 async def ILI9488_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ILI9488_A", display_ns.class_("Display_ILI9488A", DisplayDriver), cv.Schema({})
 )
 async def ILI9488A_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "ST7796", display_ns.class_("Display_ST7796", DisplayDriver), cv.Schema({})
 )
 async def ST7796_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "S3BOX", display_ns.class_("Display_S3Box", DisplayDriver), cv.Schema({})
 )
 async def S3Box_to_code(config, var):
-    return {config, var}
+    return [config, var]
 
 
 @DISPLAY_REGISTRY.register(
     "S3BOX_LITE", display_ns.class_("Display_S3BoxLite", DisplayDriver), cv.Schema({})
 )
 async def S3BoxLite_to_code(config, var):
-    return {config, var}
+    return [config, var]

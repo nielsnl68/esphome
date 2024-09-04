892c25878069d96393a5c09d266a30e84c605c65
NP v/d Spek <github_mail@lumensoft.nl>
2024-09-04 23:37:36 +0200

    try changing the speed

--- esphome/components/rtttl/rtttl.cpp
+++ esphome/components/rtttl/rtttl.cpp
@@ -16,7 +16,7 @@ static const uint16_t NOTES[] = {0,    262,  277,  294,  311,  330,  349,  370, 
                                  1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217,
                                  2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951};
 
-static const uint16_t I2S_SPEED = 1000;
+static const uint16_t I2S_SPEED = 16000;
 
 #undef HALF_PI
 static const double HALF_PI = 1.5707963267948966192313216916398;
